33c9cf9311d01c343cc0790b1621791f3cbca00a
IgorChupryna <chuprina.ihor@gmail.com>
2018-11-20 17:30:53 +0200

    springTest

--- spring_basic/src/test/java/spring/qual/CompanyTest.java
+++ spring_basic/src/test/java/spring/qual/CompanyTest.java
@@ -1,16 +1,10 @@
 package spring.qual;
 
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import spring.basic.configuration.AppConfig;
-import spring.basic.domain.autowiring.qualifier.Bond;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {Builder.class,Analytic.class,Engineer.class,Company.class})
@@ -21,11 +15,6 @@ public class CompanyTest {
 
     @Test
     public void showPayRate(){
-
-
-        System.out.println("ff");
-        //company.showRate();
-
-
+        company.showRate();
     }
 }
